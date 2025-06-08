92ee490272bdd218a22502f15b2f44d0e5c31d9c
Michelyserras <michelysrrs@gmail.com>
2025-06-07 21:23:38 -0300

    add adapter

---
+++ src/main/java/com/ifsp/atm/Utils/JsonToXmlAdapter.java
@@ -0,0 +1,43 @@
+package com.ifsp.atm.Utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class JsonToXmlAdapter {
+    public static String convert(String jsonString) throws Exception {
+        ObjectMapper jsonMapper = new ObjectMapper();
+        XmlMapper xmlMapper = new XmlMapper();
+
+        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        // Converte JSON para árvore JsonNode
+        JsonNode jsonNode = jsonMapper.readTree(jsonString);
+
+        // Converte JsonNode para XML
+        return xmlMapper.writeValueAsString(jsonNode);
+    }
+
+    public static void main(String[] args) {
+        String json = """
+            {
+                "pessoa": {
+                    "nome": "João",
+                    "idade": 30,
+                    "cidade": "São Paulo",
+                    "Genero": "Masculino",
+                    "Estado civil" : "solteiro"
+  
+                }
+            }
+        """;
+
+        try {
+            String xml = convert(json);
+            System.out.println("XML gerado:\n" + xml);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
